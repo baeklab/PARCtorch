04df7b6da5ac3df12128d6b7dd0efc163b7f374d
JosephBChoi <joseph.boogun.choi@gmail.com>
2025-05-05 09:54:09 -0400

    create test file for models

---
+++ tests/test_model.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('../')
+
+from PARCtorch.PARCv2 import PARCv2
+import torch 
+
+def test_model(model=PARCv2()): 
+    assert model.differentiator is not None
+    assert model.integrator is not None
+    assert model.loss is not None
+
+def test_PARCv2():
+    model = PARCv2()
+    
+    # model initialization
+    test_model(model) 
+
+    # check function
+    assert model.check() == 1, f"ERROR: default manuscript configuration"
+
+    model = PARCv2(loss=torch.nn.MSELoss)
+    assert model.check() == 0, f"ERROR: warning system not working properly"
