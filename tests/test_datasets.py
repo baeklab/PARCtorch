d74a651e452c728369cef28e854ebd0d3d52832a
clarencew0083 <43386076+clarencew0083@users.noreply.github.com>
2025-05-16 15:54:25 -0400

    Added unit test for acoustic scattering maze

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -136,3 +136,40 @@ def test_dataset_thewell_gsrd():
         # This dataset does not have velocity, so the last 2 channels must be zero
         assert (ic[4:, :, :] == 0.0).all()
         assert (gt[:, 4:, :, :] == 0.0).all()
+
+
+def test_dataset_thewell_asm():
+    """
+    Test WellDataset on acoustic_scattering_maze
+    """
+    future_steps = 7
+    ds = WellDatasetInterface(
+        future_steps=future_steps,
+        min_val=torch.tensor([0.0] * 6),
+        max_val=torch.tensor([1.0] * 6),
+        delta_t=1.0,
+        add_constant_scalars=True,
+        well_dataset_args={
+            "well_base_path": "/standard/sds_baek_energetic/data/physics/the_well/datasets/",
+            "well_dataset_name": "acoustic_scattering_maze",
+            "well_split_name": "test",
+            "use_normalization": False,
+        },
+    )
+    
+    field_names = [name for names in ds.well_dataset.field_names.values() for name in names]
+    print(f"Original field names in dataset: {field_names}")
+    print(f"Constant scalars in dataset: {ds.well_dataset.constant_scalar_names}")
+    print(f"Constant fields in dataset: {ds.well_dataset.constant_field_names}")
+    #assert len(ds) == 119280
+    for each in ds:
+        ic, t0, t1, gt = each
+        assert is_same_shape([5, 256, 256], ic.shape)  # 'density', 'speed_of_sound', 'pressure', 'velocity_x', 'velocity_y'
+        assert is_same_shape([], t0.shape)
+        assert is_same_shape([future_steps], t1.shape)
+        assert is_same_shape([future_steps, 5, 256, 256], gt.shape)
+        # First and second channels are the constants,
+        assert (ic[0, :, :] == ic[1, :, :]).all()
+        # assert (ic[1, :, :] == ic[1, 0, 0]).all()
+        # assert (gt[:, 1, :, :] == ic[1, 0, 0]).all()
+        # assert (gt[:, 0, :, :] == ic[0, 0, 0]).all()
